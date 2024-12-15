6ba01851eabba333c92a48b1344e9a97d2ef3fcd
DDubel <38082558+DDubel@users.noreply.github.com>
2024-12-15 01:28:09 +0100

    Tests out

--- src/components/footer-root/footer-root.component.spec.ts
+++ src/components/footer-root/footer-root.component.spec.ts
@@ -2,22 +2,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterRootComponent } from './footer-root.component';
 
-describe('FooterRootComponent', () => {
-  let component: FooterRootComponent;
-  let fixture: ComponentFixture<FooterRootComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FooterRootComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(FooterRootComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// describe('FooterRootComponent', () => {
+//   let component: FooterRootComponent;
+//   let fixture: ComponentFixture<FooterRootComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [FooterRootComponent]
+//     })
+//     .compileComponents();
+//
+//     fixture = TestBed.createComponent(FooterRootComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
